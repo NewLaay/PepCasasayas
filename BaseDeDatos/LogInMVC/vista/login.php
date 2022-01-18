ad50a7e6e45a3e485607f906154c3972348fb67d
Pep C <jcasasayasramis@iessonferrer.net>
2022-01-18 19:10:55 +0100

    commit de de mvcdeUsuarios

---
+++ BaseDeDatos/LogInMVC/vista/login.php
@@ -0,0 +1,9 @@
+<form action="controlador/CtrlLogin.php" method="POST">
+    <label for="usu">Usuario:</label>
+    <input type="text" name="usu" id="usu" autofocus>
+
+    <label for="pass">Contraseña:</label>
+    <input type="password" name="pass" id="pass">
+
+    <input type="submit" value="Iniciar sesión">
+</form>
